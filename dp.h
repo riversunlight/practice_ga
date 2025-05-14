605459baf2988221bf109f1d9583c1130a84239c
riversunlight <aabbee107@icloud.com>
2025-05-14 23:20:10 +0900

    DPのバグ解消とphenotypeとgenotypeの分離

--- dp.h
+++ dp.h
@@ -1,28 +1,41 @@
 #pragma once
 #include "knapsackProblem.h"
 using namespace std;
-class DP {
+class DP
+{
 public:
     KnapSackProblem problem;
-    DP(KnapSackProblem _problem) {
+    DP(KnapSackProblem _problem)
+    {
         problem = _problem;
     }
 
     // Return: fitness
-    int solve(int ans[]) {
+    int solve(int ans[])
+    {
         int **dp;
-        dp = (int**)malloc(sizeof(int) * (problem.n + 1));
-        for (int i = 0; i < problem.n + 1; i++) {
-            dp[i] = (int*)malloc(sizeof(int) * (problem.c_max + 1));
+        dp = (int **)malloc(sizeof(int *) * (problem.n + 1));
+        for (int i = 0; i < problem.n + 1; i++)
+        {
+            dp[i] = (int *)malloc(sizeof(int) * (problem.c_max + 1));
+            for (int j = 0; j < problem.c_max + 1; j++)
+            {
+                dp[i][j] = 0;
+            }
         }
 
-        for (int i = 0; i < problem.n; i++) {
-            for (int j = problem.c_max; j >= 0; j--) {
-                if (dp[i][j] > dp[i + 1][j]) {
+        for (int i = 0; i < problem.n; i++)
+        {
+            for (int j = problem.c_max; j >= 0; j--)
+            {
+                if (dp[i][j] > dp[i + 1][j])
+                {
                     dp[i + 1][j] = dp[i][j];
                 }
-                if (j + problem.c[i] <= problem.c_max) {
-                    if (dp[i][j] + problem.v[i] > dp[i + 1][j + problem.c[i]]) {
+                if (j + problem.c[i] <= problem.c_max)
+                {
+                    if (dp[i][j] + problem.v[i] > dp[i + 1][j + problem.c[i]])
+                    {
                         dp[i + 1][j + problem.c[i]] = dp[i][j] + problem.v[i];
                     }
                 }
@@ -30,16 +43,27 @@ public:
         }
 
         int tmp = problem.c_max;
-        for (int i = problem.n - 1; i >= 0; i--) {
-            if (tmp - problem.c[i] >= 0 && dp[i + 1][tmp] == dp[i][tmp - problem.c[i]] + problem.v[i]) {
+        for (int i = problem.n - 1; i >= 0; i--)
+        {
+            if (tmp - problem.c[i] >= 0 && dp[i + 1][tmp] == dp[i][tmp - problem.c[i]] + problem.v[i])
+            {
                 ans[i] = 1;
                 tmp -= problem.c[i];
             }
         }
+        for (int i = 0; i < problem.n; i++)
+        {
+            for (int j = 0; j < problem.c_max; j++)
+            {
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
 
         int constraint = 0;
         int eval = problem.ObjectiveFunc(ans, constraint);
-        if (constraint) {
+        if (constraint)
+        {
             cout << "DP's ans is ilegal." << endl;
         }
         return eval;
--- ga.h
+++ ga.h
@@ -2,7 +2,8 @@
 #include "knapsackProblem.h"
 using namespace std;
 
-class GA {
+class GA
+{
 public:
     KnapSackProblem problem;
     int **population;
@@ -14,88 +15,119 @@ public:
     int generation_cnt;
     int max_generation = 100;
 
-    GA(KnapSackProblem _problem) {
+    GA(KnapSackProblem _problem)
+    {
         problem = _problem;
         population_size = 20;
-        population = (int**)malloc(sizeof(int*) * population_size);
-        population_score = (int*)malloc(sizeof(int) * population_size);
+        population = (int **)malloc(sizeof(int *) * population_size);
+        population_score = (int *)malloc(sizeof(int) * population_size);
     }
 
-    void init() {
-        for (int i = 0; i < population_size; i++) {
-            population[i] = (int *)malloc(sizeof(int)*problem.n);
-            
-            for (int j = 0; j < problem.n; j++) population[i][j] = rand() % 2;
+    void init()
+    {
+        for (int i = 0; i < population_size; i++)
+        {
+            population[i] = (int *)malloc(sizeof(int) * problem.n);
+
+            for (int j = 0; j < problem.n; j++)
+                population[i][j] = rand() % 2;
             int constraint = 0;
 
-            population_score[i] = problem.ObjectiveFunc(population[i], constraint);
-            if (constraint) population_score[i] = 0;
+            int *individual_hypo;
+            individual_hypo = (int *)malloc(sizeof(int) * problem.n);
+            decoder(population[i], individual_hypo);
+            population_score[i] = problem.ObjectiveFunc(individual_hypo, constraint);
+            if (constraint)
+                population_score[i] = 0;
         }
     }
 
-    void recombine(int p1, int p2) {
+    void recombine(int p1, int p2)
+    {
         int r = rand() % problem.n;
 
-        offspring = (int**)malloc(sizeof(int*) * 2);
-        for (int i = 0; i < 2; i++) {
-            offspring[i] = (int*)malloc(sizeof(int) * problem.n);
+        offspring = (int **)malloc(sizeof(int *) * 2);
+        for (int i = 0; i < 2; i++)
+        {
+            offspring[i] = (int *)malloc(sizeof(int) * problem.n);
         }
         cout << "p1 and p2 genotype:" << endl;
-        for (int i = 0; i < problem.n; i++) cout << population[p1][i] << " ";
+        for (int i = 0; i < problem.n; i++)
+            cout << population[p1][i] << " ";
         cout << endl;
-        for (int i = 0; i < problem.n; i++) cout << population[p2][i] << " ";
+        for (int i = 0; i < problem.n; i++)
+            cout << population[p2][i] << " ";
         cout << endl;
 
-        for (int i = 0; i < problem.n; i++) {
-            if (i < r) {
+        for (int i = 0; i < problem.n; i++)
+        {
+            if (i < r)
+            {
                 offspring[0][i] = population[p1][i];
-                offspring[0][i] = population[p2][i];
-            } else {
                 offspring[1][i] = population[p2][i];
+            }
+            else
+            {
+                offspring[0][i] = population[p2][i];
                 offspring[1][i] = population[p1][i];
             }
         }
 
         cout << "offsprings: r=" << r << endl;
-        for (int i = 0; i < problem.n; i++) cout << offspring[0][i] << " ";
+        for (int i = 0; i < problem.n; i++)
+            cout << offspring[0][i] << " ";
         cout << endl;
-        for (int i = 0; i < problem.n; i++) cout << offspring[1][i] << " ";
+        for (int i = 0; i < problem.n; i++)
+            cout << offspring[1][i] << " ";
         cout << endl;
     }
 
-
     // stochastic change offspring
-    void mutate() {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < problem.n; j++) {
+    void mutate()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < problem.n; j++)
+            {
                 int r = rand() % 100;
                 if (r < 1)
-                offspring[i][j] ^= 1;
+                    offspring[i][j] ^= 1;
             }
         }
     }
 
     // stochastic
-    void select_parents(int &p1, int &p2) {
-        for (int i = 0; i < 2; i++) {
-            int* histogram = (int*)malloc(sizeof(int) * (population_size + 1));
+    void select_parents(int &p1, int &p2)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            int *histogram = (int *)malloc(sizeof(int) * (population_size + 1));
             histogram[0] = 0;
-            for (int j = 0; j < population_size; j++) {
-                if (j == p1) continue;
-                if (j == p1 + 1) {
+            for (int j = 0; j < population_size; j++)
+            {
+                if (j == p1)
+                    continue;
+                if (j == p1 + 1)
+                {
                     histogram[j + 1] = histogram[j - 1] + population_score[j] + 1;
                 }
                 histogram[j + 1] = histogram[j] + population_score[j] + 1;
             }
             int sum = histogram[population_size];
-            if (histogram[population_size] == 0) sum = histogram[population_size - 1];
+            if (histogram[population_size] == 0)
+                sum = histogram[population_size - 1];
             cout << "sum:" << sum << endl;
             int r = rand() % sum;
-            for (int j = 0; j < population_size; j++) {
-                if (j == p1) continue;
-                if (r < histogram[j + 1]) {
-                    if (i == 0) p1 = j;
-                    else {
+            for (int j = 0; j < population_size; j++)
+            {
+                if (j == p1)
+                    continue;
+                if (r < histogram[j + 1])
+                {
+                    if (i == 0)
+                        p1 = j;
+                    else
+                    {
                         p2 = j;
                     }
                     break;
@@ -103,31 +135,61 @@ public:
             }
         }
     }
-    
-    void eval_offspring() {
-        offspring_score = (int*)malloc(sizeof(int) * 2);
-        for (int i = 0; i < 2; i++) {
+
+    void decoder(int genotype[], int phenotype[])
+    {
+        int c_sum = 0;
+        for (int i = 0; i < problem.n; i++)
+        {
+            if (genotype[i] == 1 && c_sum + problem.c[i] <= problem.c_max)
+            {
+                phenotype[i] = 1;
+                c_sum += problem.c[i];
+            }
+            else
+            {
+                phenotype[i] = 0;
+            }
+        }
+    }
+
+    void eval_offspring()
+    {
+        offspring_score = (int *)malloc(sizeof(int) * 2);
+        for (int i = 0; i < 2; i++)
+        {
             int constraint = 0;
-            offspring_score[i] = problem.ObjectiveFunc(offspring[i], constraint);
-            if (constraint) offspring_score[i] = 0;
+            int *off_hypo;
+            off_hypo = (int *)malloc(sizeof(int) * problem.n);
+            decoder(offspring[i], off_hypo);
+            offspring_score[i] = problem.ObjectiveFunc(off_hypo, constraint);
+            if (constraint)
+                offspring_score[i] = 0;
         }
     }
 
     // deterministic
-    void select_next_generation() {
+    void select_next_generation()
+    {
         cout << "ga:" << endl;
         int l1 = -1, l2 = -1; // worst individual of population
-        for (int i = 0; i < population_size; i++) {
+        for (int i = 0; i < population_size; i++)
+        {
             int idx = i;
-            if (l1 == -1) l1 = idx;
-            else {
-                if (population_score[idx] < population_score[l1]) {
+            if (l1 == -1)
+                l1 = idx;
+            else
+            {
+                if (population_score[idx] < population_score[l1])
+                {
                     int tmp = l1;
                     l1 = idx;
                     idx = tmp;
                 }
-                if (l2 == -1) l2 = idx;
-                else if (population_score[idx] < population_score[l2]) {
+                if (l2 == -1)
+                    l2 = idx;
+                else if (population_score[idx] < population_score[l2])
+                {
                     int tmp = l2;
                     l2 = idx;
                     idx = tmp;
@@ -138,10 +200,12 @@ public:
         cout << offspring_score[0] << " " << offspring_score[1] << endl;
         cout << population_score[l1] << " " << population_score[l2] << endl;
         int best_offspring = 0;
-        if (offspring_score[0] > offspring_score[1]) {
+        if (offspring_score[0] > offspring_score[1])
+        {
             best_offspring = 1;
         }
-        if (offspring_score[best_offspring] > population_score[l2]) {
+        if (offspring_score[best_offspring] > population_score[l2])
+        {
             population[l1] = population[l2];
             population_score[l1] = population_score[l2];
             population[l2] = offspring[best_offspring];
@@ -149,27 +213,30 @@ public:
             l1 = l2;
             best_offspring ^= 1;
         }
-        if (offspring_score[best_offspring] > population_score[l1]) {
+        if (offspring_score[best_offspring] > population_score[l1])
+        {
             population[l1] = offspring[best_offspring];
             population_score[l1] = offspring_score[best_offspring];
         }
-
     }
 
-    int termination_condition() {
-        if (generation_cnt >= max_generation) return 1;
+    int termination_condition()
+    {
+        if (generation_cnt >= max_generation)
+            return 1;
         return 0;
     }
 
-
-    int solve(int *ans) {
+    int solve(int *ans)
+    {
         init();
         generation_cnt = 0;
 
         int best_idx = -1;
         int best_score = 0;
-        int* score_log = (int*)malloc(sizeof(int)*(max_generation + 10));
-        while(!termination_condition()) {
+        int *score_log = (int *)malloc(sizeof(int) * (max_generation + 10));
+        while (!termination_condition())
+        {
             cout << "generation_cnt: " << generation_cnt << endl;
             int p1 = -1, p2 = -1;
             select_parents(p1, p2);
@@ -182,32 +249,37 @@ public:
             best_idx = -1;
             best_score = -100000;
 
-            for (int i = 0; i < population_size; i++) {
-                if (population_score[i] > best_score) {
+            for (int i = 0; i < population_size; i++)
+            {
+                if (population_score[i] > best_score)
+                {
                     best_idx = i;
                     best_score = population_score[i];
                 }
             }
 
-            
             cout << "best_score:" << best_score << endl;
-            for (int i = 0; i < problem.n; i++) {
+            for (int i = 0; i < problem.n; i++)
+            {
                 cout << population[best_idx][i] << " ";
             }
             cout << endl;
             score_log[generation_cnt] = best_score;
             generation_cnt++;
-
-            
         }
-        for (int i = 0; i < problem.n; i++) {
-            ans[i] = population[best_idx][i];
+        int *best_solution;
+        best_solution = (int *)malloc(sizeof(int) * problem.n);
+        decoder(population[best_idx], best_solution);
+        for (int i = 0; i < problem.n; i++)
+        {
+            ans[i] = best_solution[i];
         }
 
         ofstream writing_file;
 
         writing_file.open("score_generation.txt");
-        for (int i = 0; i < max_generation; i++) {
+        for (int i = 0; i < max_generation; i++)
+        {
             writing_file << score_log[i] << endl;
         }
         writing_file.close();
